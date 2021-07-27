bbe58204923145317178b73192aed872d5fcbcb6
idWebMada <repository@idweb.space>
2021-07-27 13:52:11 +0300

    Page home design

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, Input, Renderer2 } from '@angular/core';
+import { DomController } from '@ionic/angular';
 
 @Directive({
   selector: '[appHideHeader]'
 })
-export class HideHeaderDirective {
+export class HideHeaderDirective implements AfterViewInit {
+  @Input('appHideHeader') header: any;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2,
+    private domCtrl: DomController
+  ) { }
+  ngAfterViewInit(): void { 
+  }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  categories = [];
+  highlights = [];
+  featured = [];
 
-  constructor() {}
+  catSlideOpts = {
+    slidesPerView: 3.5,
+    spaceBetween: 10,
+    slidesOffsetBefore: 11,
+    freeMode: true
+  };
+
+  highlightSlideOpts =  {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true
+  };
+
+  featuredSlideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.Data();  
+  }
+
+  Data() {
+    this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json').subscribe((res: any) => { 
+      this.categories = res.categories;
+      this.highlights = res.highlights;
+      this.featured = res.featured;
+    });
+  }
+
+  doRefresh(event) {
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
 
 }
